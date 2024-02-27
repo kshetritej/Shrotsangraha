5caa380ee6371e9729f7f948bd4f7a2e0e8d2bfe
kshetritej <tejghartikshetri@gmail.com>
2024-02-27 22:40:33 +0545

    add:loger and notifier util decoy

--- server/src/configs/env.config.ts
+++ server/src/configs/env.config.ts
@@ -10,6 +10,10 @@ class EnvEnvironment {
     static DB_PASSWORD=process.env.DB_PASSWORD;
     static DB_TYPE=process.env.DB_TYPE;
     static DB_PORT=process.env.DB_PORT || 5432;
+
+    //email configuratin
+    static EMAIL_USERNAME=process.env.EMAIL_USERNAME;
+    static EMAIL_PASSSWORD=process.env.MAIL_PASSSWORD;
 }
 
 export default EnvEnvironment;
